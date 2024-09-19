d9f9bb38b8063052764e3e85684783c24c4638a1
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-09-19 17:50:33 -0300

    corrigindo

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename="/portfolio">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
