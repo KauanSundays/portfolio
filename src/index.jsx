fe3b54b6081ef0de41fe96d49ac88341ff8d029a
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-09-19 17:24:08 -0300

    WIP 1

--- src/index.jsx
+++ src/index.jsx
@@ -4,16 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-
+// Definindo o caminho base para a subpasta do GitHub Pages
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router basename="/portfolio">
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vials
+// Se você quiser começar a medir o desempenho em seu aplicativo, passe uma função
+// para registrar resultados (por exemplo: reportWebVitals(console.log))
+// ou envie para um endpoint de análise. Saiba mais: https://bit.ly/CRA-vitals
 reportWebVitals();
